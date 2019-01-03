a529f38403e63d5e7b32fe424e12440f844061c8
bambandiaye181 <bambandiaye181@gmail.com>
2019-01-03 02:54:28 +0100

    Initial commit

---
+++ src/environment/environment.ts
@@ -0,0 +1,17 @@
+//you should replace this values with yours
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBuXqxHWgb7_rYongL_dW5Tfv5nqJHsyhg",
+    authDomain: "sos-services-app.firebaseapp.com",
+    databaseURL: "https://sos-services-app.firebaseio.com",
+    projectId: "sos-services-app",
+    storageBucket: "sos-services-app.appspot.com",
+    messagingSenderId: "332510555760"
+  },
+  google_web_client_id: "332510555760-qmib55ic9u07f2qb6e1cvkv8jc7ebagr.apps.googleusercontent.com",
+  facebook_app_id: 2045656788850074,
+  wordpress_url: 'https://wordpress.startapplabs.com/blog/',
+  wordpress_rest_api_url: 'https://wordpress.startapplabs.com/blog/wp-json/wp/v2/'
+};
